35523787729fa1e46adb24e9c47f4c5d75c23daf
ivanosquis10 <ivanosquis10.12@gmail.com>
2023-11-03 00:50:49 -0400

    blocked the button of the form

--- src/components/contact-form/contact-form.tsx
+++ src/components/contact-form/contact-form.tsx
@@ -49,7 +49,7 @@ export const ContactForm = () => {
         {((errors.message?.message) != null) && <Error message={errors.message.message} />}
       </div>
 
-        <Button className='font-bold'>
+        <Button className='font-bold' disabled>
           Enviarme mensaje
         </Button>
 
