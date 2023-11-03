e353c60559d66571f4b07142c8fc2bc295e790fc
ivanosquis10 <ivanosquis10.12@gmail.com>
2023-11-03 11:57:54 -0400

    example

--- src/services/email.ts
+++ src/services/email.ts
@@ -1,8 +1,8 @@
 import { send } from '@emailjs/browser'
 
-// const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY as string
-// const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID as string
-// const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID as string
+const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY as string
+const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID as string
+const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID as string
 
 type Props = {
   name: string
@@ -12,6 +12,12 @@ type Props = {
 
 export const sendContactMessage = async ({ name, email, message }: Props) => {
   try {
+    console.log(
+      PUBLIC_KEY,
+      TEMPLATE_ID,
+      SERVICE_ID
+    )
+
     const data = await send(process.env.NEXT_PUBLIC_SERVICE_ID!, process.env.NEXT_PUBLIC_TEMPLATE_ID!, {
       user_name: name,
       message,
