2d8a9f09e063774d340981a95256b1a2c17bd333
ivanosquis10 <ivanosquis10.12@gmail.com>
2023-11-07 18:11:00 -0400

    [refactoring]: Restructured folder structure for the converter, history and shared components

--- src/app/conversor/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loading } from '@/components'
-
-export default function LoadingPage() {
-  return (
-    <div className='grid w-full h-screen max-w-6xl mx-auto place-content-center'>
-      <Loading />
-    </div>
-  )
-}
