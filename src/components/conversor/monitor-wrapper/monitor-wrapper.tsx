55d41e00ccd952854a126a6a746033e9ee47fd7c
ivanosquis10 <ivanosquis10.12@gmail.com>
2023-11-23 12:56:18 -0400

    [added]: the error page has been added to make it easier to manage

--- src/components/conversor/monitor-wrapper/monitor-wrapper.tsx
+++ src/components/conversor/monitor-wrapper/monitor-wrapper.tsx
@@ -1,18 +1,17 @@
 import { getAllRates } from '@/services/rates'
 import { MonitorCard, ErrorPage } from '@/components'
-
 export async function MonitorWrapper() {
   const data = await getAllRates()
 
   if (!data.success) return <ErrorPage />
   return (
-    <div className='relative p-2 space-y-2 overflow-y-auto border rounded dark:border-zinc-800 h-fit max-h-[230px] lg:max-h-[450px] dark:bg-zinc-900/70 bg-zinc-200 shadow-md'>
-      {data.success && data.monitors.map((monitor) => (
-        <MonitorCard
-          key={monitor.title}
-          monitor={monitor}
-        />
-      ))}
-    </div>
+      <div className='relative p-2 space-y-2 overflow-y-auto border rounded dark:border-zinc-800 h-fit max-h-[230px] lg:max-h-[450px] dark:bg-zinc-900/70 bg-zinc-200 shadow-md'>
+        {data.success && data.monitors.map((monitor) => (
+          <MonitorCard
+            key={monitor.title}
+            monitor={monitor}
+          />
+        ))}
+      </div>
   )
 }
