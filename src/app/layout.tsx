05f4d3466d6cb44933614630a1fb24f0eaa2c3db
ivanosquis10 <ivanosquis10.12@gmail.com>
2024-02-01 12:52:53 -0400

    [add]: vercel analytics was added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,65 +1,59 @@
-import './globals.css'
-import Script from 'next/script'
-import { Toaster } from 'react-hot-toast'
-import type { Metadata } from 'next'
+import "./globals.css"
+import type { Metadata } from "next"
 
-import { Footer, Header, ThemeProvider } from '@/components'
-import { config, umami } from '@/app/config'
-import { onest } from '@/fonts'
+import Script from "next/script"
+import { Analytics } from "@vercel/analytics/react"
+import { Toaster } from "react-hot-toast"
+
+import { Footer, Header, ThemeProvider } from "@/components"
+import { config, umami } from "@/app/config"
+import { onest } from "@/fonts"
 
 export const metadata: Metadata = {
   metadataBase: new URL(config.meta.siteUrl),
   title: {
     default: config.meta.title,
-    template: `%s | ${config.meta.title}`
+    template: `%s | ${config.meta.title}`,
   },
   description: config.meta.description,
   openGraph: {
     title: config.meta.title,
     description: config.meta.description,
-    url: './',
+    url: "./",
     siteName: config.meta.title,
     images: [config.meta.socialBanner],
-    locale: 'en_US',
-    type: 'website'
+    locale: "en_US",
+    type: "website",
   },
   twitter: {
     title: config.meta.title,
-    card: 'summary_large_image',
-    images: [config.meta.socialBanner]
+    card: "summary_large_image",
+    images: [config.meta.socialBanner],
   },
   creator: config.author.name,
   publisher: config.author.name,
   authors: [
     {
-      name: 'Ivan Rodríguez',
-      url: config.author.website
-    }
+      name: "Ivan Rodríguez",
+      url: config.author.website,
+    },
   ],
-  viewport: 'width=device-width, initial-scale=1'
+  viewport: "width=device-width, initial-scale=1",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${onest.className} overflow-x-hidden relative scroll-smooth min-h-screen`}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-              {children}
-            <Footer />
-            <Toaster position='top-right' />
-          </ThemeProvider>
+      <body className={`${onest.className} relative min-h-screen overflow-x-hidden scroll-smooth`}>
+        <ThemeProvider disableTransitionOnChange enableSystem attribute="class" defaultTheme="dark">
+          <Header />
+          {children}
+          <Analytics />
+          <Footer />
+          <Toaster position="top-right" />
+        </ThemeProvider>
       </body>
-      <Script
-        async
-        src={umami.src}
-        data-website-id={umami.website_id}
-      />
+      <Script async data-website-id={umami.website_id} src={umami.src} />
     </html>
   )
 }
